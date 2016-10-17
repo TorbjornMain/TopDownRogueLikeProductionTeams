8b981638cb07ab79fbea1c199098e07783a3cf87
Riley Huppatz <riley.huppatz@ad.aie.edu>
2016-10-17 13:28:33 +1030

    The UI things

---
+++ Assets/mainMenuButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class mainMenuButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void changeColour()
+    {
+        GetComponent<RawImage>().color = new Color32(255,255,255,100);
+    }
+}
---
+++ Assets/mainMenuUI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class mainMenuUI : MonoBehaviour {
+
+    //public GameObject mainMenu;
+    public Texture mainMenu;
+    private bool imageSwapped = false;
+    public GameObject startGamePanel;
+    public GameObject optionsPanel;
+    public GameObject quitPanel;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (imageSwapped == false && Input.anyKey)
+        {
+            GetComponent<RawImage>().texture = mainMenu;
+            startGamePanel.SetActive(true);
+            optionsPanel.SetActive(true);
+            quitPanel.SetActive(true);
+        }
+    }
+}
