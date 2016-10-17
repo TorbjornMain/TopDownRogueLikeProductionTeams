35d2d1948178e64b7210b61f39512bb55c3b82ae
Riley Huppatz <riley.huppatz@ad.aie.edu>
2016-10-18 10:23:20 +1030

    Main Menu UI now with options menu

---
+++ Assets/Scripts/GUI/BackButton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BackButton : MonoBehaviour {
+    public RawImage logoPanel;
+    public Texture replacementLogo;
+    public GameObject startGamePanel;
+    public GameObject optionsPanel;
+    public GameObject quitPanel;
+    public GameObject controlsPanel;
+    public GameObject backPanel;
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void BackPressed()
+    {
+        logoPanel.GetComponent<RawImage>().texture = replacementLogo;
+        startGamePanel.SetActive(true);
+        optionsPanel.SetActive(true);
+        quitPanel.SetActive(true);
+        controlsPanel.SetActive(false);
+        backPanel.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/GUI/OptionsButton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OptionsButton : MonoBehaviour {
+
+    public RawImage logo;
+    public Texture options;
+    public GameObject startGamePanel;
+    public GameObject optionsPanel;
+    public GameObject quitPanel;
+    public GameObject controlsPanel;
+    public GameObject backPanel;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OptionsPressed()
+    {
+        logo.GetComponent<RawImage>().texture = options;
+        startGamePanel.SetActive(false);
+        optionsPanel.SetActive(false);
+        quitPanel.SetActive(false);
+        controlsPanel.SetActive(true);
+        backPanel.SetActive(true);
+    }
+}
