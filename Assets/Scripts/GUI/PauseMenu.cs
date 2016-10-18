6b21df3053ddfeb42c7852034f4f1d66b193f480
Riley Huppatz <riley.huppatz@ad.aie.edu>
2016-10-18 10:59:20 +1030

    Pause Menu does a thing

---
+++ Assets/Scripts/GUI/PauseMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+    public GameObject pauseMenu;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetButtonDown("Cancel"))
+        {
+            print("escape pushed");
+            ToggleActive();
+        }
+	}
+
+    void ToggleActive()
+    {
+        if (pauseMenu.activeInHierarchy == true)
+        {
+            pauseMenu.SetActive(false);
+        }
+        else
+        {
+            pauseMenu.SetActive(true);
+        }
+    }
+}
