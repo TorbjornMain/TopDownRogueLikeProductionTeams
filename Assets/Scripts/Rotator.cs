f1b8df3c5370fc0aaa75f419439b45dd3d263059
James Buttfield <james.buttfield@ad.aie.edu>
2016-10-18 11:33:04 +1030

    added many assets to the project

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+//This script will run constantly to any object it is attached to
+public class Rotator : MonoBehaviour
+{
+    //Update is called as often as possible
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+    }
+}
