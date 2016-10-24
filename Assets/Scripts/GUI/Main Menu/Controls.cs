11ddbe14997d574534f6c3504fab07041c1e1c7a
Riley Huppatz <riley5694@gmail.com>
2016-10-24 13:17:21 +1030

    Ui folder changes and a few tweaks and bugfixes

---
+++ Assets/Scripts/GUI/Main Menu/Controls.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Controls : MonoBehaviour {
+    public RawImage logo;
+    public Texture controls;
+    public GameObject controlsPanel;
+    public GameObject backPanel;
+    public GameObject controlsBackPanel;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ControlsPressed()
+    {
+        logo.GetComponent<RawImage>().texture = controls;
+        controlsPanel.SetActive(false);
+        backPanel.SetActive(false);
+        controlsBackPanel.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/GUI/Main Menu/ControlsBackButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ControlsBackButton : MonoBehaviour {
+    public RawImage logo;
+    public Texture options;
+    public GameObject controlsPanel;
+    public GameObject backPanel;
+    public GameObject controlsBackPanel;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void BackPressed()
+    {
+        logo.GetComponent<RawImage>().texture = options;
+        controlsPanel.SetActive(true);
+        backPanel.SetActive(true);
+        controlsBackPanel.SetActive(false);
+    }
+}
