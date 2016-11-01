49c6fc22b089f47253cc3b5ce69dda791502d973
Zachary De Maria <ddewerg@gmail.com>
2016-11-01 10:32:24 +1030

    Preparing for merge

---
+++ Assets/Scripts/Animation/LegAnimationScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Transport))]
+[RequireComponent(typeof(Animator))]
+public class LegAnimationScript : MonoBehaviour {
+
+	private Transport t;
+	private Animator a;
+
+	// Use this for initialization
+	void Start () {
+		t = GetComponent<Transport> ();
+		a = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		a.SetFloat ("Speed", t.transportSpeed); 
+	}
+}
--- Assets/Scripts/Mobility/Transport.cs
+++ Assets/Scripts/Mobility/Transport.cs
@@ -27,9 +27,21 @@ public class Transport : EquippableItem {
 
 	public TransportStats stats;
 
+	[System.NonSerialized]
+	public float transportSpeed;
+
+
+	private Vector3 previousPos;
+
+
+	void Start()
+	{
+		previousPos = transform.position;
+	}
 
 	public void Drive(Vector3 direction)
 	{
+		transportSpeed = (transform.position - previousPos).magnitude;
 		if (direction.magnitude > 0) {
 			footRotVec = (direction.normalized + footRotVec).normalized;
 			transform.rotation = Quaternion.Euler (0, Quaternion.FromToRotation (new Vector3 (0, 0, 1), Vector3.Slerp (transform.forward, footRotVec, stats.turnRate)).eulerAngles.y, 0);
@@ -38,5 +50,6 @@ public class Transport : EquippableItem {
 		} else if (mainBody.velocity.magnitude > 0) {
 			Quaternion.Euler (0, Quaternion.FromToRotation (new Vector3 (0, 0, 1), Vector3.Slerp (transform.forward, mainBody.velocity, stats.turnRate * 2)).eulerAngles.y, 0);
 		}
+		previousPos = transform.position;
 	}
 }
