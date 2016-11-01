8724dd1712a13d05f41c202c70f6df0f532cb6af
Zachary De Maria <ddewerg@gmail.com>
2016-11-02 10:11:24 +1030

    First Test of gun cards

--- Assets/Scripts/GUI/GunCardOverlayWidget.cs
+++ Assets/Scripts/GUI/GunCardOverlayWidget.cs
@@ -2,6 +2,34 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class GunCardOverlayWidget : MonoBehaviour {
+[RequireComponent(typeof(PrimaryWeaponSystem))]
+public class GunCardOverlayWidget :	OverlayWidget {
+	PrimaryWeaponSystem pws;
+	WeaponStatCard sc;
+
+	protected override void Start () {
+		base.Start ();
+		pws = GetComponent<DamageableItem> ();
+		if ((sc = widgetInstance.GetComponent<WeaponStatCard> ()) == null) {
+			sc.Name = pws.Stats.name;
+			sc.Damage = "Damage: " & pws.Stats.damage.ToString ();
+			sc.Spread = "Spread: " & pws.Stats.spread.ToString ();
+			sc.NumberProjectiles = "Number of Projectiles: " & pws.Stats.numProjectiles.ToString ();
+			sc.FireRate = "Fire Rate: " & pws.Stats.speed.ToString ();
+			sc.Description = pws.Stats.description;
+		}
+		sc.gameObject.SetActive (false);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		sc.gameObject.SetActive (true);
+	}
+
+
+	void OnTriggerExit(Collider other)
+	{
+		sc.gameObject.SetActive (false);
+	}
 
 }
