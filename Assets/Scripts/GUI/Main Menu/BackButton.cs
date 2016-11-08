01fa2d6050a8b665999a7837d7d5a30a02130a46
Riley Huppatz <riley5694@gmail.com>
2016-11-08 20:48:08 +1030

    Entire Menu UI is working and fixed!

--- Assets/Scripts/GUI/Main Menu/BackButton.cs
+++ Assets/Scripts/GUI/Main Menu/BackButton.cs
@@ -3,14 +3,8 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class BackButton : MonoBehaviour {
-    public GameObject startGamePanel;
-    public GameObject optionsPanel;
-    public GameObject quitPanel;
-    public GameObject controlsPanel;
-    public GameObject backPanel;
-    public GameObject musicSlider;
-    public GameObject soundSlider;
-    public GameObject imageEmpty;
+    public GameObject menuEmpty;
+    public GameObject optionsEmpty;
 
     // Use this for initialization
     void Start () {
@@ -24,13 +18,7 @@ public class BackButton : MonoBehaviour {
 
     public void BackPressed()
     {
-        startGamePanel.SetActive(true);
-        optionsPanel.SetActive(true);
-        quitPanel.SetActive(true);
-        imageEmpty.SetActive(true);
-        controlsPanel.SetActive(false);
-        backPanel.SetActive(false);
-        musicSlider.SetActive(false);
-        soundSlider.SetActive(false);
+        menuEmpty.SetActive(true);
+        optionsEmpty.SetActive(false);
     }
 }
--- Assets/Scripts/GUI/Main Menu/OptionsButton.cs
+++ Assets/Scripts/GUI/Main Menu/OptionsButton.cs
@@ -4,14 +4,9 @@ using System.Collections;
 
 public class OptionsButton : MonoBehaviour {
 
-    public GameObject images;
-    public GameObject startGamePanel;
-    public GameObject optionsPanel;
-    public GameObject quitPanel;
-    public GameObject controlsPanel;
-    public GameObject backPanel;
-    public GameObject musicSlider;
-    public GameObject soundSlider;
+    public GameObject optionsEmpty;
+    public GameObject menuEmpty;
+
 
     // Use this for initialization
     void Start () {
@@ -25,13 +20,7 @@ public class OptionsButton : MonoBehaviour {
 
     public void OptionsPressed()
     {
-        startGamePanel.SetActive(false);
-        optionsPanel.SetActive(false);
-        quitPanel.SetActive(false);
-        controlsPanel.SetActive(true);
-        backPanel.SetActive(true);
-        musicSlider.SetActive(true);
-        soundSlider.SetActive(true);
-        images.SetActive(false);
+        optionsEmpty.SetActive(true);
+        menuEmpty.SetActive(false);
     }
 }
