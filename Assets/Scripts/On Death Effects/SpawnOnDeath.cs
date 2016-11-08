8702ed923a9304214fffd7900a786078592b89a3
Zachary De Maria <ddewerg@gmail.com>
2016-11-08 15:14:13 +1030

    Started SpawnOnDeath framework for explosions and pickups

---
+++ Assets/Scripts/On Death Effects/SpawnOnDeath.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(DamageableItem))]
+public class SpawnOnDeath : MonoBehaviour {
+	public GameObject objectToSpawn;
+	public Vector3 spawnOffset;
+
+	void Die()
+	{
+		GameObject o = Instantiate<GameObject> (objectToSpawn);
+		o.transform.position = transform.TransformPoint (spawnOffset);
+	}
+}
+
