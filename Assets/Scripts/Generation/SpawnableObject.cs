5a0f3af4fff9a3bde51561a4a24d8dc913564b60
Zachary DeMaria <zachary.demaria@gmail.com>
2016-11-10 15:40:48 +1030

    small fixes and started woking on dissapear on death

--- Assets/Scripts/Generation/SpawnableObject.cs
+++ Assets/Scripts/Generation/SpawnableObject.cs
@@ -2,12 +2,13 @@
 using System.Collections;
 using System.Collections.Generic;
 
-
+[DisallowMultipleComponent]
 public class SpawnableObject : MonoBehaviour, System.IComparable<SpawnableObject> {
 
 	public uint width = 1, height = 1;
 	public uint levelClass = 0;
 	public float heightOffset = 0.5f;
+	public float frequency = 1;
 	[System.NonSerialized]
 	public LevelManager lm;
 
---
+++ Assets/Scripts/On Death Effects/DissapearOnDeath.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(DamageableItem))]
+public class DissapearOnDeath : MonoBehaviour {
+
+	void Die()
+	{
+
+	}
+}
