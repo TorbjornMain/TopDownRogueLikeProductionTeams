4eac0faed07abb21cd5f4b31098b7e04c49adacc
Riley Huppatz <riley5694@gmail.com>
2016-11-12 11:22:47 +1030

    Sound effects and sound slider work on main menu

---
+++ Assets/Scripts/GUI/Main Menu/SoundSlider.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SoundSlider : MonoBehaviour
+{
+    float volume = 1f;
+    Slider soundSlider;
+    // Use this for initialization
+    void Start()
+    {
+        soundSlider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SaveSliderValue()
+    {
+        volume = soundSlider.value;
+        PlayerPrefs.SetFloat("SoundSliderVolumeLevel", volume);
+    }
+}
---
+++ Assets/Scripts/GUI/SoundFXAudioLevel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundFXAudioLevel : MonoBehaviour {
+
+    AudioSource sound;
+    // Use this for initialization
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+        sound.volume = PlayerPrefs.GetFloat("SoundSliderVolumeLevel", sound.volume);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
