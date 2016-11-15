6e928bb4b29917762805183920f3190330926b1b
Riley Huppatz <riley5694@gmail.com>
2016-11-15 18:01:26 +1030

    HUD health is nearly coded

---
+++ Assets/Scripts/GUI/HUD/HUDManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDManager : MonoBehaviour {
+    public GameObject player;
+    public GameObject healthBar;
+    HealthBar health;
+    float playerHealth = -1;
+
+    // Use this for initialization
+    void Start () {
+        health = healthBar.GetComponent<HealthBar>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+        if(player != null && playerHealth != -1)
+        {
+
+        }
+        health.percentFilled = playerHealth;
+    }
+}
